2780caecee400a36f8fb20b9ce7c92f612b65d1f
Germainkip <kiplangat.ngeno2019@gmail.com>
2022-06-02 13:25:38 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
+/**
+ *  * main - prints the message using puts
+ *   * Return: 0 if no error is encountered
+ *    */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
