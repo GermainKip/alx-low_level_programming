57e217301addcfcbfc0a3197eef277647acaee56
Germainkip <kiplangat.ngeno2019@gmail.com>
2022-06-03 11:34:23 -0700

    Alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ *
+ *Return: Always 0 if it is successful
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <='z'; ch++)
+	{
+	   putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
