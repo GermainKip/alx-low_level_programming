193198218d4dc725dae751f2b1a290a19732c7e7
Germainkip <kiplangat.ngeno2019@gmail.com>
2022-06-07 10:54:20 -0700

    Guideline

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+int _putchar(char m);
+int main(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int a);
+int print_last_digit(int b);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif
