50e7c227b6fd659bccebf04ebba1ab872f411e2a
Germainkip <kiplangat.ngeno2019@gmail.com>
2022-06-12 03:59:01 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
 /**
- * main - prints the message and adds new line 
- *Return: 0 if no error is encountered
+ * main - prints the text
+ *
+ * Return: 0 if it is successful
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-	    
-}	
+}
